fa520fdff4e2324266ef7956de7d1413a3ca6515
Dustin <clerin+gitkraken@gmail.com>
2019-12-06 09:14:28 -0800

    finished ex2

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,9 +1,7 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 def get_indices_of_item_weights(weights, length, limit):
@@ -33,7 +31,6 @@ def get_indices_of_item_weights(weights, length, limit):
         else:
             i += 1
 
-
     return None
 
 
@@ -42,8 +39,3 @@ def print_answer(answer):
         print(str(answer[0] + " " + answer[1]))
     else:
         print("None")
-
-# print(get_indices_of_item_weights([9,1],2,10))
-# print(get_indices_of_item_weights([4, 6, 10, 15, 16],5,21))
-# print(get_indices_of_item_weights([4,4],2,8))
-# print(get_indices_of_item_weights([12, 6, 7, 14, 19, 3, 0, 25, 40],9,7))
